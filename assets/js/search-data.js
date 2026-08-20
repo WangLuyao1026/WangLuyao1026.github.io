b0287f62f0a6d7edb79cbf80037634e1880cdf44
WangLuyao1026 <522025110043@smail.nju.edu.cn>
2026-08-20 14:53:40 +0000

    Deploying to gh-pages from @ WangLuyao1026/WangLuyao1026.github.io@657cff870bd8cc7cdb9c03d9c2e7d4474d3ef1eb 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -32,7 +32,7 @@ ninja.data = [{
           },
         },{id: "nav-research",
           title: "Research",
-          description: "学术研究 | Academic Research",
+          description: "学术研究与项目 | Academic Research",
           section: "Navigation",
           handler: () => {
             window.location.href = "/publications/";
